b13cd1e29ef539f6868972d19666e5ebf0b41302
hereroCoder <83869107+hereroCoder@users.noreply.github.com>
2023-12-15 17:55:58 +0000

    fixed card prices to two decimal prices

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -9,7 +9,7 @@ const ProductCard = (props) => {
         <img src={props.imgURL} alt={props.name} className='w-2/4'/>
         <p className="leading-7 text-xl">{props.name}</p>
         <p className="leading-7 text-xl">{props.size}</p>
-        <p className="leading-7 text-xl font-semibold">  £ {props.price}</p>
+        <p className="leading-7 text-xl font-semibold">  £ {props.price.toFixed(2)}</p>
         <button className="invisible bg-green-600 p-2 rounded text-sm font-semibold group-hover:visible duration-300 text-white" onClick={()=>addItem(props)}> ADD TO CART</button>
       </div>
       
