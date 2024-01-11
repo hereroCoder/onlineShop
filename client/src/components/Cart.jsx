0f2b5972418f08a3644bf0565b11d1538c3b66ac
hereroCoder <83869107+hereroCoder@users.noreply.github.com>
2024-01-11 11:58:54 +0000

    cart formatted properly

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -19,32 +19,34 @@ const Cart = () => {
     return (
       <div className="m-auto text-center">
           <h1 className="font-semibold">Your Cart is Empty</h1>
-          <a href="#self-care" className="bg-green-400 px-6 py-2 hover:scale-105 transition ease-linear ">Shop</a>
+          <a href="#self-care" className="bg-blue px-6 py-2 hover:scale-105 transition ease-linear ">Shop</a>
       </div>
  
     ) 
   } 
 
   return (
-    <section className="py-4 container font-lg">
+    <section className="py-4 container font-lg w-2/4 m-auto">
       <div className="flex flex-col justify-center items-center pr-2">
         {/* items */}
-        <div className="relative overflow-x-auto">
+        <div className="relative overflow-x-auto w-full">
           <table className="w-full text-left color-black">
            {items.map((item,index)=>{
             return(
                <tbody className=" border-gray-200 border">
+             
                 <tr key={index} >
                   <td>
                     <img src={item.imgURL} alt="" className="h-14" />
                   </td>
                   <td className="py-8 px-2">{item.name} ({item.size}) </td>
                   <td className="py-8 px-2 flex">
-                    <button className="bg-red-500 px-2 mr-2 rounded font-bold text-white" onClick={()=>(updateItemQuantity(item.id,item.quantity-1))}>-</button>
+                    <button className="bg-transparent px-2 mr-2 border-blue rounded font-bold text-blue border-2 " onClick={()=>(updateItemQuantity(item.id,item.quantity-1))}>-</button>
                     {item.quantity}
-                    <button className="bg-green-500 px-2 ml-2 rounded font-bold text-white" onClick={()=>(updateItemQuantity(item.id, item.quantity+1))}>+</button>
+
+                    <button className="bg-blue px-2 ml-2 rounded font-bold text-white hover:shadow-2xl shadow-black" onClick={()=>(updateItemQuantity(item.id, item.quantity+1))}>+</button>
                   </td>
-                   <td className="py-8 px-2 font-semibold"> £{item.price.toFixed(2)}</td>
+                  <td className="py-8 px-2 "> £{item.price.toFixed(2)}</td>
 
                   {/* delete button per item */}
                   {/* <td>
@@ -64,20 +66,22 @@ const Cart = () => {
            })} 
 
           </table>
+
+        </div>
+        <div className=" flex w-full justify-between px-4">
+           <p>Total: </p>
+           <p  className="font-semibold pr-4"> £ {cartTotal.toFixed(2)}</p>
         </div>
         {/* total, checkout, and clear */}
 
-        <div className="flex items-center gap-6 pt-2">
-          <button className="border font-semibold border-red-500 text-red-500 px-2 py-1 rounded hover:font-bold" onClick={()=>(
+        <div className="flex items-center w-full justify-between gap-6 pt-4">
+          <button className="border border-red-500 text-red-500 px-2 py-1 rounded hover:font-bold" onClick={()=>(
             emptyCart()
           )} >Empty Cart</button>
 
-           <button className="px-2 py-1 bg-green-200 rounded hover:font-bold" onClick={()=>(
+           <button className="px-4 py-2 hover:border-2 hover:border-blue hover:bg-transparent bg-orange rounded " onClick={()=>(
             alert('Sorry, checkout page not implemented yet.')
           )}>Checkout</button>
-         
-
-          <p>Total: <span  className="font-semibold"> £ {cartTotal.toFixed(2)}</span></p>
           
          
 
--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -1,14 +1,16 @@
 
 const Newsletter = () => {
   return (
-    <section className=" newsletter justify-around items-center text-center py-10 gap-4 bg-slate-100 pl-2  ">
+    <section className="newsletter justify-around items-center text-center py-10 gap-4 bg-slate-100 pl-2  ">
+      
       <h3 className="font-bold text-center">Sign up for our latest offers</h3>
       <p>You'll be among the first to hear about our latest products and offers, like our amazing Specialbuys.</p>
+
       <div className="flex justify-between items-center border-2 pl-2  rounded-full  m-auto md:w-2/6 mb-2">
         <input type="email" required placeholder="Enter your email" className="outline-none bg-transparent" />
-        <button className="bg-orange text-black hover:scale-105 rounded-full px-4 py-2 ">Subscribe</button>
-
+        <button className="bg-orange text-black hover:scale-105 rounded-full px-4 py-2 font-semibold">Subscribe</button>
       </div>
+
       <p>To find out about how we use your information please visit our <a href="" className="underline">Privacy Notice</a></p>
       
     </section>
